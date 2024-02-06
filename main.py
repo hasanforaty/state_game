5e2332dc1e325a450a729e4b7b1f2713c73db706
hasan foraty <hasanforaty19@gmail.com>
2024-02-06 14:19:50 +0330

    add creating csv file of state not answered

--- main.py
+++ main.py
@@ -20,26 +20,32 @@ def game_over():
     game_on = False
 
 
-screen.listen()
-screen.onkey(key='q', fun=game_over)
+guss_set = set()
 
-correct_answers = 0
 while game_on:
     user_input = screen.textinput(
-        title=f'{correct_answers}/50 States Correct',
+        title=f'{len(guss_set)}/50 States Correct',
         prompt="what's another state name ?",
     )
     if user_input is None:
         continue
+    if user_input.lower() == 'exit':
+        game_over()
     result = states.query(f"state.str.lower() == '{user_input.lower()}'")
     if not result.empty:
         result = result.iloc[0]
-        correct_answers += 1
+        guss_set.add(user_input.lower())
         state_name = turtle.Turtle()
         state_name.hideturtle()
         state_name.penup()
-        print(result)
         state_name.goto(result[X_COLUMN], result[Y_COLUMN])
         state_name.write(result[STATE_COLUMN], align='center', font=('Arial', 10, 'bold'))
 
-screen.exitonclick()
+full_list = states.state.to_list()
+
+un_Answer = []
+for state in full_list:
+    if state.lower() not in guss_set:
+        un_Answer.append(state)
+un_Answer_file = pandas.Series(un_Answer)
+un_Answer_file.to_csv('un_Answer.csv', index=False)
